54ef63e8c887d9c17ebcd9f1590d82dd472fdbcd
Tom McKechnie <tommckechnie8@gmail.com>
2024-11-25 11:30:59 +0000

    Add favicon, and final tidy ups for the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,9 +15,13 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 
+    document.getElementById("answer-box").addEventListener("keydown", function(event){
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+    })
+
     runGame("addition")
-    // runGame("subtract")
-    // runGame("multiply")
 });
 
 /**
@@ -26,14 +30,17 @@ document.addEventListener("DOMContentLoaded", function() {
  */
 function runGame(gameType){
 
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // Creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
-    // } else if (gameType === "subtract") {
-    //     displaySubtractQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
     } else {
@@ -80,9 +87,12 @@ function calculateCorrectAnswer(){
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+        
     } else if (operator === "x") {
         return [operand1 * operand2, "multiply"];
-    } else  {
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -118,9 +128,9 @@ function displayAdditionQuestion(operand1, operand2){
 
 function displaySubtractQuestion(operand1, operand2){
 
-    // document.getElementById('operand1').textContent = operand1;
-    // document.getElementById('operand2').textContent = operand2;
-    // document.getElementById('operator').textContent = "-";
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
